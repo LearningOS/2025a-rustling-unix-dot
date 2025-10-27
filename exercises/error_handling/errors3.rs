ae2e2b85f451381e1c3267f59acc3b05106a37fe
Fang Yuhan <fangyuhan153@qq.com>
2025-10-27 19:27:04 +0800

    // errors3.rs // // This is a program that is trying to use a completed version of the // `total_cost` function from the previous exercise. It's not working though! // Why not? What should we do to fix it? // // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a // hint.
    // I AM NOT DONE
    
    use std::num::ParseIntError;
    
    // 将 main 函数的返回类型改为 Result<(), ParseIntError>
    // 这样可以使用 ? 运算符传播错误
    fn main() -> Result<(), ParseIntError> {
        let mut tokens = 100;
        let pretend_user_input = "8";
    
        let cost = total_cost(pretend_user_input)?; // 现在 ? 运算符可以正常工作
    
        if cost > tokens {
            println!("You can't afford that many!");
        } else {
            tokens -= cost;
            println!("You now have {} tokens.", tokens);
        }
    
        Ok(()) // 成功时返回 Ok(())
    }
    
    pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
        let processing_fee = 1;
        let cost_per_item = 5;
        let qty = item_quantity.parse::<i32>()?;
    
        Ok(qty * cost_per_item + processing_fee)
    }

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -11,11 +11,13 @@
 
 use std::num::ParseIntError;
 
-fn main() {
+// 将 main 函数的返回类型改为 Result<(), ParseIntError>
+// 这样可以使用 ? 运算符传播错误
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input)?; // 现在 ? 运算符可以正常工作
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -23,6 +25,8 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(()) // 成功时返回 Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
