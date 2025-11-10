66c0bad18345a456af959b45481f02576d03aca9
Fang Yuhan <fangyuhan153@qq.com>
2025-11-10 19:36:57 +0800

    // iterators1.rs // // When performing operations on elements within a collection, iterators are // essential. This module helps you get familiar with the structure of using an // iterator and how to go through elements within an iterable collection. // // Make me compile by filling in the `???`s // // Execute `rustlings hint iterators1` or use the `hint` watch subcommand for a // hint.
    // I AM NOT DONE
    fn main() {
        let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
    
        let mut my_iterable_fav_fruits = my_fav_fruits.iter();   // Step 1：创建不可变迭代器
    
        assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
        assert_eq!(my_iterable_fav_fruits.next(), Some(&"custard apple"));     // Step 2：迭代器下一个元素
        assert_eq!(my_iterable_fav_fruits.next(), Some(&"avocado"));
        assert_eq!(my_iterable_fav_fruits.next(), Some(&"peach"));     // Step 3：迭代器下一个元素
        assert_eq!(my_iterable_fav_fruits.next(), Some(&"raspberry"));
        assert_eq!(my_iterable_fav_fruits.next(), None);     // Step 4：迭代器耗尽，返回 None
    }

--- exercises/iterators/iterators1.rs
+++ exercises/iterators/iterators1.rs
@@ -10,16 +10,15 @@
 // hint.
 
 // I AM NOT DONE
-
 fn main() {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
 
-    let mut my_iterable_fav_fruits = ???;   // TODO: Step 1
+    let mut my_iterable_fav_fruits = my_fav_fruits.iter();   // Step 1：创建不可变迭代器
 
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 2
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"custard apple"));     // Step 2：迭代器下一个元素
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"avocado"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 3
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"peach"));     // Step 3：迭代器下一个元素
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"raspberry"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 4
+    assert_eq!(my_iterable_fav_fruits.next(), None);     // Step 4：迭代器耗尽，返回 None
 }
