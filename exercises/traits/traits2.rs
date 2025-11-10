7be1ba20dea32dafe37b5fa685218ab5f354d008
Fang Yuhan <fangyuhan153@qq.com>
2025-11-10 20:03:52 +0800

    // traits2.rs // // Your task is to implement the trait `AppendBar` for a vector of strings. To // implement this trait, consider for a moment what it means to 'append "Bar"' // to a vector of strings. // // No boiler plate code this time, you can do this! // // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
    // I AM NOT DONE
    
    trait AppendBar {
        fn append_bar(self) -> Self;
    }
    
    // 为Vec<String>实现AppendBar trait
    impl AppendBar for Vec<String> {
        fn append_bar(mut self) -> Self {
            self.push(String::from("Bar"));
            self
        }
    }
    
    #[cfg(test)]
    mod tests {
        use super::*;
    
        #[test]
        fn is_vec_pop_eq_bar() {
            let mut foo = vec![String::from("Foo")].append_bar();
            assert_eq!(foo.pop().unwrap(), String::from("Bar"));
            assert_eq!(foo.pop().unwrap(), String::from("Foo"));
        }
    }

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -14,7 +14,13 @@ trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+// 为Vec<String>实现AppendBar trait
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -27,3 +33,4 @@ mod tests {
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
 }
+
